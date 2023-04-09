2c88e18b4d459d72c7231282fdf218ddbead4d92
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-04-09 15:29:33 -0500

    added bootstrap styled a little and changed some fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import { useState, useRef } from "react";
 import "./App.css";
-import { animated, useSpring } from "@react-spring/web";
+import { HiOutlinePlusCircle } from "react-icons/hi";
+
 import { motion } from "framer-motion";
 
 function App() {
-  const springs = useSpring({
-    from: { x: 0 },
-    to: { x: 100 },
-  });
-
   const [choice, setChoice] = useState("");
   const [dinnerTonight, setDinnerTonight] = useState([
-    { name: "La Hacienda", weight: 0.5 },
-    { name: "Jack's", weight: 0.5 },
-    { name: "McDonalds", weight: 0.5 },
-    { name: "306 BBQ", weight: 0.5 },
-    { name: "Waffle House", weight: 0.5 },
-    { name: "Rice Box", weight: 0.5 },
-    { name: "Texas Roadhouse", weight: 0.5 },
-    { name: "Taco Mama", weight: 0.5 },
-    { name: "Stanfields", weight: 0.5 },
+    { name: "Tacos", weight: 0.5 },
+    { name: "Lasagna", weight: 0.5 },
+    { name: "Chicken Pockets", weight: 0.5 },
+    { name: "Grilled Cheese", weight: 0.5 },
+    { name: "Pancakes", weight: 0.5 },
+    { name: "Chicken and Rice", weight: 0.5 },
+    { name: "Korean Beef", weight: 0.5 },
+    { name: "Steak and Potatoes", weight: 0.5 },
+    { name: "Frozen Pizza", weight: 0.5 },
   ]);
 
   const handleRange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -36,25 +32,44 @@ function App() {
   };
 
   return (
-    <div className="container">
-      {" "}
-      <motion.div style={{ margin: "5px", justifyContent: "center" }}>
-        <motion.h1 layout transition={{ type: "spring" }} drag whileDrag={{ scale: 0.75 }} dragMomentum={false}>
-          {choice}
-        </motion.h1>
-      </motion.div>
-      {dinnerTonight.map((rs, i) => (
-        <div className="container">
-          <div key={`restaurant-div-${i}`}>
-            <label>{rs.name}</label>
-            - <input onChange={handleRange} name={rs.name} type="range" step={0.05} value={rs.weight} max={1} /> +
+    <div className="container text-center">
+      <h1 className="titlefont">What's For dinner?</h1>
+      <div className="row justify-content-center">
+        <motion.div>
+          <motion.h1
+            layout
+            transition={{ type: "spring", delay: 1 }}
+            drag
+            whileDrag={{ scale: 0.75 }}
+            dragMomentum={false}
+            className="titlefont m-5 justify-content-center"
+          >
+            {choice}
+          </motion.h1>
+        </motion.div>{" "}
+        {dinnerTonight.map((rs, i) => (
+          <div key={`restaurant-div-${i}`} className="flex flex-row col-7">
+            <label htmlFor="customRange1" className="form-label">
+              {rs.name}
+            </label>
+
+            <input
+              className=" form-range"
+              onChange={handleRange}
+              name={rs.name}
+              type="range"
+              step={0.05}
+              value={rs.weight}
+              max={1}
+              id="customRange1"
+            />
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
       <motion.button
-        style={{ margin: "5px" }}
         onClick={!choice ? calculateRandomChoice : (e) => setChoice("")}
         whileTap={{ scale: 0.95 }}
+        className="m-5 btn btn-dark"
       >
         {!choice ? "Get a random Choice" : <span>Choose Again?</span>}
       </motion.button>
